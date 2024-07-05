bf04b1663973331130af77d01a3989f0a9138311
sammy23454 <samuellilian72@gmail.com>
2024-07-05 00:32:56 +0000

    hypersdk

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "LILISAMUEL-HYPERCHAIN"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "LILI"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "LIL"
 )
 
 var ID ids.ID
